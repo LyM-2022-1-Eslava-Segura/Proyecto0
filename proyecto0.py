81a2fb8c739ddffeac04fe1c8c710adcd68945d4
Juan Andrés Eslava Tovar <88861582+juaneslava@users.noreply.github.com>
2022-02-21 01:07:50 -0500

    comandos terminados, faltan estructuras de control

--- proyecto0.py
+++ proyecto0.py
@@ -8,6 +8,11 @@ funciones = []
 
 almacenamiento = {}
 
+posiciones=[':around',':left', ':right']
+posiciones2=[':front',':left', ':right',':back']
+puntos_cardinales=[':north',':east',':west',':south']
+objetos=['Balloons', 'Chips']
+
 def main (archivo):
     
     for linea in archivo:
@@ -28,7 +33,7 @@ def recorrido(linea):
     lista_linea = linea.split(' ')
     lista_linea[0]= lista_linea[0].strip('\n')
     solucion = False
-    #boolean = ["BLOCKEDP","!BLOCKEDP", " BLOCKEDP", " !BLOCKEDP"]
+    boolean = ["BLOCKEDP","!BLOCKEDP", " BLOCKEDP", " !BLOCKEDP"]
     var = (lista_linea[0].strip(")"))
     
         
@@ -50,78 +55,72 @@ def recorrido(linea):
             solucion
             
         
-    elif lista_linea[0] == 'RIGHT':
-        respuesta = right(lista_linea,variables)
+    elif lista_linea[0] == '(=':
+        respuesta = equal(lista_linea,variables)
         if respuesta == True:
             solucion = True
         else:
             solucion
             
                 
-    elif lista_linea[0] == 'LEFT':
-        respuesta = left(lista_linea,variables)
-        if respuesta == True:
-            solucion = True
-        else:
-            solucion
-            
-        
-    elif lista_linea[0] == 'ROTATE':
-        respuesta = rotate(lista_linea,variables)
+    elif lista_linea[0] == '(turn':
+        respuesta = turn(lista_linea,variables)
         if respuesta == True:
             solucion = True
         else:
             solucion
             
                 
-    elif lista_linea[0] == 'LOOK':
-        respuesta = look(lista_linea)
+    elif lista_linea[0] == '(face':
+        respuesta = face(lista_linea)
         if respuesta == True:
             solucion = True
         else:
             solucion
             
                 
-    elif lista_linea[0] == 'DROP':
-        respuesta = drop(lista_linea,variables)
+    elif lista_linea[0] == '(put':
+        respuesta = put(lista_linea,variables)
         if respuesta == True:
             solucion = True
         else:
             solucion
             
                 
-    elif lista_linea[0] == 'FREE':
-        respuesta = free(lista_linea,variables)
-        if respuesta == True:
-            solucion = True
-        else:
-            solucion
-            
-        
-    elif lista_linea[0] == 'PICK':
+    elif lista_linea[0] == '(pick':
         respuesta = pick(lista_linea,variables)
         if respuesta == True:
             solucion = True
         else:
             solucion
-            
-                
-    elif lista_linea[0] == 'POP':
-        respuesta = pop(lista_linea,variables)
+    
+    elif lista_linea[0] == '(move-dir':
+        respuesta = move_dir(lista_linea,variables)
         if respuesta == True:
             solucion = True
         else:
             solucion
-            
-        
-    elif lista_linea[0] == 'CHECK':
-        respuesta = check(lista_linea,variables)
+
+    elif lista_linea[0] == '(run-dirs':
+        respuesta = run_dirs(lista_linea,variables)
         if respuesta == True:
             solucion = True
         else:
             solucion
-            
-                
+
+    elif lista_linea[0] == '(move-face':
+        respuesta = move_face(lista_linea,variables)
+        if respuesta == True:
+            solucion = True
+        else:
+            solucion
+    elif lista_linea[0] == '(skip':
+        respuesta = skip(lista_linea)
+        if respuesta == True:
+            solucion = True
+        else:
+            solucion    
+
     elif lista_linea[0] == 'IF' and (lista_linea[1] in boolean) and (lista_linea[2][0] == "["):
         respuesta = if_command(linea)
         if respuesta == True:
@@ -129,12 +128,6 @@ def recorrido(linea):
         else:
             solucion
                         
-    elif lista_linea[0] == 'NOP':
-        respuesta = nop(lista_linea)
-        if respuesta == True:
-            solucion = True
-        else:
-            solucion
             
     elif lista_linea[0] == '(BLOCK' or lista_linea[0] == '( BLOCK':
         respuesta = block(linea)
@@ -181,7 +174,7 @@ def defvar(lista:list, variables:dict):
     
 
 
-# Función para comando 'MOVE'
+# Función para comando 'move'
 def move(lista:list, variables:dict):
     tamanio = len(lista)
     respuesta = False
@@ -197,146 +190,131 @@ def move(lista:list, variables:dict):
     
     return respuesta
 
-# Función para comando 'RIGHT'
-def right(lista:list,variables:dict):
+
+# Función para =
+def equal(lista:list, variables:dict):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 3:
+        lista[2]= lista[2].strip('\n')
+        lista[2]=lista[2].strip(')')
+        if (lista[2].isdigit() == True) and (lista[1].isdigit() == False):
+            respuesta = True
+            variables[lista[1]] = lista[2]
+    return respuesta
+
+
+# Función para comando 'turn'
+def turn(lista:list,variables:dict):
     tamanio = len(lista)
     respuesta = False
     if tamanio == 2:
         lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
+        lista[1]= lista[1].lstrip(")")
+        if (lista[1] in posiciones):
+            respuesta = True   
+    return respuesta
+
+
+# Función para comando 'face'
+def face(lista:list):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 2:
+        lista[1]= lista[1].strip('\n')
+        lista[1]= lista[1].strip(')')
+        if lista[1] in puntos_cardinales:
+            respuesta = True  
+    return respuesta
+
+# Función para comando 'put X n'
+def put(lista:list,variables:dict):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 3:
+        lista[2]= lista[2].strip('\n')
+        lista[2]= lista[2].lstrip(")")
+        if lista[2].isdigit() == True and lista[1] in objetos:
             respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
+        elif (lista[1].isdigit() == False) and (lista[1] in objetos):
             for elemento in variables:
                 if lista[1] == elemento:
                     respuesta = True
     
     return respuesta
 
-# Función para comando 'LEFT'
-def left(lista:list,variables:dict):
-    tamanio = len(lista)
-    respuesta = False
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
-            respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
-            for elemento in variables:
-                if lista[1] == elemento:
-                    respuesta = True
-    
-    return respuesta
-
-# Función para comando 'ROTATE'
-def rotate(lista:list,variables:dict):
-    tamanio = len(lista)
-    respuesta = False
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
-            respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
-            for elemento in variables:
-                if lista[1] == elemento:
-                    respuesta = True
-    
-    return respuesta
-
-# Función para comando 'LOOK'
-def look(lista:list):
-    tamanio = len(lista)
-    respuesta = False
-    lista2= ('N','E','W','S')
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        if lista[1] in lista2:
-            respuesta = True
-    
-    return respuesta
-
-# Función para comando 'DROP'
-def drop(lista:list,variables:dict):
-    tamanio = len(lista)
-    respuesta = False
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
-            respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
-            for elemento in variables:
-                if lista[1] == elemento:
-                    respuesta = True
-    
-    return respuesta
-
-# Función para comando 'FREE'
-def free(lista:list,variables:dict):
-    tamanio = len(lista)
-    respuesta = False
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
-            respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
-            for elemento in variables:
-                if lista[1] == elemento:
-                    respuesta = True
-    
-    return respuesta
-
-# Función para comando 'PICK'
+# Función para comando 'pick X n'
 def pick(lista:list,variables:dict):
     tamanio = len(lista)
     respuesta = False
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
-            respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
-            for elemento in variables:
-                if lista[1] == elemento:
-                    respuesta = True
-    
-    return respuesta
-
-# Función para comando 'POP'
-def pop(lista:list,variables:dict):
-    tamanio = len(lista)
-    respuesta = False
-    if tamanio == 2:
-        lista[1]= lista[1].strip('\n')
-        lista[1]= lista[1].lstrip(":")
-        if lista[1].isdigit() == True:
-            respuesta = True
-        elif (lista[1].isdigit() == False) and (lista[1].islower()== True):
-            for elemento in variables:
-                if lista[1] == elemento:
-                    respuesta = True
-    
-    return respuesta
-
-# Función para comando 'CHECK'
-def check(lista:list,variables:dict):
-    tamanio = len(lista)
-    respuesta = False
-    lista2= ['C','B']
     if tamanio == 3:
         lista[2]= lista[2].strip('\n')
-        lista[2]= lista[2].lstrip(":")
-        if (lista[2].isdigit() == True) and (lista[1] in lista2):
+        lista[2]= lista[2].lstrip(")")
+        if lista[2].isdigit() == True and lista[1] in objetos:
             respuesta = True
-        elif (lista[2].isdigit() == False) and (lista[2].islower()== True) and (lista[1] in lista2):
+        elif (lista[1].isdigit() == False) and (lista[1] in objetos):
             for elemento in variables:
-                if lista[2] == elemento:
+                if lista[1] == elemento:
                     respuesta = True
     
     return respuesta
+
+
+#Función para 'move-dir n D'
+def move_dir(lista:list,variables:dict):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 3:
+        lista[2]= lista[2].strip('\n')
+        lista[2]= lista[2].lstrip(")")
+        if lista[1].isdigit() == True and lista[2] in posiciones2:
+            respuesta = True
+        elif (lista[1].isdigit() == False) and (lista[2] in posiciones2):
+            for elemento in variables:
+                if lista[1] == elemento:
+                    respuesta = True
+    
+    return respuesta
+
+#Función para 'run-dirs Ds'
+def run_dirs(lista:list,variables:dict):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 2:
+        lista[1]= lista[1].strip('\n')
+        lista[1]= lista[1].lstrip(")")
+        lista[1]=list(lista[1])
+        if (isinstance(lista[1], list) == True) and len(lista[1]) > 0:
+            for direccion in lista[1]:
+                if direccion in posiciones2:
+                    respuesta = True
+    return respuesta
+
+#Función para 'move-face n O'
+def move_face(lista:list,variables:dict):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 3:
+        lista[2]= lista[2].strip('\n')
+        lista[2]= lista[2].lstrip(")")
+        if lista[1].isdigit() == True and lista[2] in puntos_cardinales:
+            respuesta = True
+        elif (lista[1].isdigit() == False) and (lista[2] in puntos_cardinales):
+            for elemento in variables:
+                if lista[1] == elemento:
+                    respuesta = True 
+    return respuesta
+
+
+
+# Función para comando 'skip'
+def skip(lista:list):
+    tamanio = len(lista)
+    respuesta = False
+    if tamanio == 1:
+        respuesta = True  
+    return respuesta
+
 
 # Función para comando 'IF'
 def if_command(linea:str):
@@ -588,14 +566,7 @@ def to_end(linea:str,lista:list):
     return respuesta
             
 
-# Función para comando 'NOP'
-def nop(lista:list):
-    tamanio = len(lista)
-    respuesta = False
-    if tamanio == 1:
-        respuesta = True
-    
-    return respuesta
+
 
 main(archivo)
 archivo.close()
